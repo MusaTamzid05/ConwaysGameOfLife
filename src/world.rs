b49ca653372b69bfac667495b4d488c5f22c3abe
MusaTamzid05 <tamzid101@gmail.com>
2025-03-31 03:55:20 +0600

    Add cell update rules(without loop)

--- src/world.rs
+++ src/world.rs
@@ -21,7 +21,9 @@ impl World {
 
         }
 
+        cells[10][9].alive = true;
         cells[10][10].alive = true;
+        cells[10][11].alive = true;
 
         Self { size, cells }
     }
@@ -40,30 +42,68 @@ impl World {
 
 
     fn update(&mut self) {
-        let mut alive_data_list: Vec<(i32, i32)> = Vec::new();
+        let mut target_cell_data_list : Vec<(i32, i32, bool)> = Vec::new();
 
         for row in 0..self.size {
             for col in 0..self.size {
                 let cell: &Cell = &self.cells[row as usize][col as usize];
-                if cell.alive {
-                    alive_data_list.push((row, col));
+                let alive_score: i32 = self.get_alive_score_of(cell);
+                let alive: bool = cell.alive;
+
+                if alive_score <= 1 && alive  {
+                    // not enough neighbours, die !!
+                    target_cell_data_list.push((row, col, false));
+                }
+
+                else if alive_score >= 2 && alive_score <= 3  && alive  {
+                    // just enough neighbours, survice
+                    target_cell_data_list.push((row, col, true));
+                }
+
+                else if alive_score > 3  && alive  {
+                    // overpolulation, must die
+                    target_cell_data_list.push((row, col, false));
+                }
+
+                else if alive_score ==  3 && alive == false {
+                    // enough neighbours, time to be born !!
+                    target_cell_data_list.push((row, col, true));
                 }
             }
         }
 
-        for (row, col) in alive_data_list {
-            let cell: &Cell = &self.cells[row as usize][col as usize];
-            let neighbour_info_list: Vec<(i32, i32)> = cell.get_neighbour_info_list();
+        for (row, col, alive) in target_cell_data_list {
+            self.cells[row as usize][col as usize].alive = alive;
 
-            for (n_row, n_col) in neighbour_info_list {
-                self.cells[n_row as usize][n_col as usize].alive = true;
-            }
 
 
         }
 
 
 
+    }
+
+    pub fn get_alive_score_of(&self, cell: &Cell) -> i32 {
+        let mut alive_score: i32 = 0;
+            let neighbour_info_list: Vec<(i32, i32)> = cell.get_neighbour_info_list();
+
+            for (n_row, n_col) in neighbour_info_list {
+                let mut valid: bool = false;
+
+                if (n_row >= 0 && n_row < self.size) && (n_col >= 0 && n_col< self.size) {
+                    valid = true;
+                }
+
+                if valid == false {
+                    continue;
+                }
+
+                if self.cells[n_row as usize][n_col as usize].alive {
+                    alive_score += 1;
+                }
+            }
+
+        alive_score
     }
 
     pub fn start(&mut self) {
