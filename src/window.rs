31a2b87bf781fd23d96bdbd6cf42a84911c84be5
MusaTamzid05 <tamzid101@gmail.com>
2025-04-03 21:37:42 +0600

    Add dynamic cell activation feature

--- src/window.rs
+++ src/window.rs
@@ -4,6 +4,7 @@ use sdl2::video::Window;
 use sdl2::event::Event;
 use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 use std::time::Duration;
 
 use crate::world::World;
@@ -77,6 +78,39 @@ impl MWindow {
         self.canvas.present();
 
 
+    }
+
+    pub fn handle_cell_activation(&mut self, mouse_x: i32, mouse_y: i32) {
+        let cells: &mut Vec<Vec<Cell>> = &mut self.world.cells;
+        let size: i32 = i32::try_from(self.rect_size).unwrap();
+
+        for cell_row in cells.iter_mut() {
+            for cell in cell_row.iter_mut() {
+                let min_y: i32 = cell.row * size;
+                let min_x: i32 = cell.col * size;
+                let max_x: i32 = min_x + size;
+                let max_y: i32 = min_y + size;
+
+                let mut valid: bool = false;
+
+                if mouse_x >= min_x && mouse_x <=  max_x && mouse_y >= min_y && mouse_y <= max_y {
+                    valid = true;
+                }
+
+                if valid == false {
+                    continue;
+                }
+
+
+                if cell.alive == false {
+                    cell.alive = true;
+                }
+
+
+
+            }
+        }
+
 
     }
 
@@ -89,7 +123,17 @@ impl MWindow {
                     Event::Quit {..} => break 'running,
                     Event::KeyDown { keycode: Some(Keycode::Return), ..} => {
                         self.world.update();
-                    }
+                    },
+                    Event::MouseButtonDown { x, y, mouse_btn, ..} => {
+                        match mouse_btn {
+                            MouseButton::Left => {
+                                self.handle_cell_activation(x, y);
+
+                            }
+                            _=>{}
+                        }
+
+                    },
                     _=> {}
                 }
 
